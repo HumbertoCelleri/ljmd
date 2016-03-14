0677302a53848b1b126653b185b59e9b6f7033d3
jimsrc <jimmy.ilws@gmail.com>
2016-03-14 20:43:42 -0300

    tt2.py funciona, por algun motivo raro. No se si c.idxlist significa algo

--- src/tt2.py
+++ src/tt2.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 import ctypes as C
 CLIB = C.CDLL('./mylib.so')
+
+#--- area() 
 CLIB.area.argtypes = [C.Structure]
 CLIB.area.restype  = C.c_int
 
@@ -9,15 +11,24 @@ CLIB.area.restype  = C.c_int
 class cell_t(C.Structure):
     _fields_ = [ ('natoms', C.c_int),
                  ('owner', C.c_int),  
-                 ('idxlist', C.c_void_p )
+                 ('idxlist', C.POINTER(C.c_int) ) # funciona ok
+                 #('idxlist', C.pointer(C.c_int()) ) #
                 ]
-
+    
+    """
     def __init__(self, natoms, owner, idxlist):
-        self.natoms = natoms
-        self.owner = owner
-        self.idxlist = (C.c_int * natoms)(*idxlist)
-
+        print " this init... "
+        #self.natoms = natoms
+        #self.owner = owner
+        #self.idxlist = (C.c_int * natoms)(*idxlist)
+    """
+    
     def area(self):
         return CLIB.area(self)
 
+#---- test of above
+import numpy as np
 
+aa = np.ones(3)
+aaa = aa.ctypes.data_as(C.POINTER(C.c_int))
+c = cell_t(3, 4, aaa)

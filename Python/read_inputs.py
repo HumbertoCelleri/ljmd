f60ba7b78b81c2a5dbb9591ec299467e12c4cb6c
Rodrigo Lugones <rodrigo@rlugones>
2016-03-15 08:33:45 -0300

    add_inputs: Pone los inputs en la clase sys y lee el archivo restart para ver las posiciones y velocidades iniciales.

--- Python/read_inputs.py
+++ Python/read_inputs.py
@@ -5,17 +5,24 @@ import scipy.io as spio
 import time
 
 def read_inputs ( case , path ) :
-  ifile = open(path+'/examples/')
-  natoms = int(ifile.readline().split(' ')[0])
-  mass = float(ifile.readline().split(' ')[0]) # in AMU
-  epsilon = float(ifile.readline().split(' ')[0]) # in kcal/mol
-  sigma = float(ifile.readline().split(' ')[0]) # in angstrom
-  rcut = float(ifile.readline().split(' ')[0]) # in angstrom
-  box_length = float(ifile.readline().split(' ')[0]) # in angstrom
-  restart = ifile.readline().split(' ')[0] # filename for restart
-  trajectory = ifile.readline().split(' ')[0] # filename for trajectories
-  energies = ifile.readline().split(' ')[0] # filename for energies
-  MD_steps = int(ifile.readline().split(' ')[0]) # number of MD steps
-  MD_time_step = float(ifile.readline().split(' ')[0]) # MD time step (in fs)
-  output_frec = int(ifile.readline().split(' ')[0]) # output print frequency
-  ifile.close()
+    ifile = open(path+'/examples/'+case+'.inp','r')
+    sys.natoms = int(ifile.readline().split(' ')[0]) # Number of atoms
+    sys.mass = float(ifile.readline().split(' ')[0]) # Mass in AMU
+    sys.epsilon = float(ifile.readline().split(' ')[0]) # Epsilon in kcal/mol
+    sys.sigma = float(ifile.readline().split(' ')[0]) # Sigma in angstrom
+    sys.rcut = float(ifile.readline().split(' ')[0]) # Cut-off radius in angstrom
+    sys.box = float(ifile.readline().split(' ')[0]) # Box length in angstrom
+    restfile = ifile.readline().split(' ')[0] # Filename for restart
+    trajfile = ifile.readline().split(' ')[0] # Filename for trajectories
+    ergfile = ifile.readline().split(' ')[0] # Filename for energies
+    sys.nsteps = int(ifile.readline().split(' ')[0]) # Number of MD steps
+    sys.dt = float(ifile.readline().split(' ')[0]) # MD time step (in fs)
+    nprint = int(ifile.readline().split(' ')[0]) # Output print frequency
+    ifile.close()
+    
+    data=np.loadtxt(restfile)
+    sys.pos=np.reshape(data[:sys.natoms,:],[sys.natoms*3],'F')
+    sys.vel=np.reshape(data[sys.natoms:,:],[sys.natoms*3],'F')
+    del data
+    
+    return sys, trajfile, ergfile, nprint

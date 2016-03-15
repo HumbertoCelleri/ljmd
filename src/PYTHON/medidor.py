744b60ff9cefc214824602e6115d541f1e9ce314
Cuenta Generica de Fisica <wtpc-16@ws5.labo2>
2016-03-15 17:17:41 -0300

    Archivos .py agregados

---
+++ src/PYTHON/medidor.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import ctypes as C
+CLIB = C.CDLL('./libc.so')
+
+
+class Medidor(object):
+
+    def __init__(self):
+        pass
+
+    def kinetic_energy(self, sys):
+
+        CLIB. ekin(C.byref(sys))
+
+        return sys.ekin
+
+
+    def potencial_energy(self, sys):
+    
+        CLIB.force(C.byref(sys))
+
+        return sys.epot
+
+
+    def temperature(self, sys):
+
+        return sys.temp
+
+
+    def get_pos(self, sys):
+
+        return sys.pos
+
+
+    def get_vel(self, sys):
+
+        return sys.vel
+
+
+
+
+
+
+
+
---
+++ src/PYTHON/sys.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import ctypes as C
+CLIB = C.CDLL('./libc.so')
+
+
+class cell_t(C.Structure):
+""" Declaro la estructura cell_t definida en el codigo C """
+
+    _fields_ = [ ('natoms', C.c_int),
+                 ('owner', C.c_int),
+                 ('idxlist', C.POINTER(C.c_int))]
+
+class mdsys_t(C.Structure):
+""" Declaro la estructura asociada al sistema definida en C """
+
+    _fields_ = [ ('dt', C.c_double),
+                 ('mass', C.c_double),
+                 ('epsilon', C.c_double),
+                 ('sigma', C.c_double),
+                 ('box', C.c_double),
+                 ('rcut', C.c_double),
+                 ('ekin', C.c_double),
+                 ('epot', C.c_double),
+                 ('temp', C.c_double),
+                 ('_pad1', C.c_double),
+                 ('pos', C.POINTER(C.c_double)),
+                 ('vel', C.POINTER(C.c_double)),
+                 ('frc', C.POINTER(C.c_double)),
+                 ('clist', C.POINTER(cell_t)),
+                 ('plist', C.POINTER(C.c_int)),
+                 ('_pad2', C.c_int),
+                 ('natoms', C.c_int),
+                 ('nfi', C.c_int),
+                 ('nsteps', C.c_int),
+                 ('nthreads', C.c_int),
+                 ('ngrid', C.c_int),
+                 ('ncell', C.c_int ),
+                 ('npair', C.c_int),
+                 ('nidx', C.c_int),
+                 ('delta', C.c_double) ]
+                 
+
+    def __init__(self):
+        pass
+
+
+    def evolution(self, pasos = 1):
+
+    """ Metodo evolucion: el sistema evoluciona mediante el algoritmo de verlet
+    la cantidad de pasos especificada. cellfreq se refiere a los pasos en los que 
+    actualiza la lista de vecinos (definido en el codigo C) """
+
+        cellfreq = 4
+        for i in range(0, pasos):
+
+             CLIB.velverlet(byref(self))
+
+             if i % cellfreq == 0:
+                  CLIB.updcells(byref(self))
+
+             
+    def input(self, fname):
+    """ Agregar funcion input de datos """
+
+        pass
+             
+
+
+        
+
